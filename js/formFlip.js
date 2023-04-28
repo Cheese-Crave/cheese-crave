10c77e018d642227fd763df096f00f289f8cf55e
Mary Kohn <marymkohn@gmail.com>
2023-04-28 17:29:40 -0400

    start landing page button

--- js/formFlip.js
+++ js/formFlip.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const flipButtons = document.querySelectorAll('.flip-button');
+    const signUpButton = document.querySelectorAll('.sign-up');
+
     flipButtons.forEach(function (button) {
         button.addEventListener('click', flipForm);
     });
+
+    signUpButton.forEach(function (button) {
+        button.addEventListener('click', showForm);
+    });
+
+    closeButton.forEach(function (button) {
+        button.addEventListener('click', hideForm);
+    })
 });
+
+function showForm() {
+    const flipForm = document.getElementById()
+}
 
 function flipForm() {
     const flipElement = document.querySelector('.flip');
