f9633435fe029ed7b327832660b60df579f2736e
Chyna D <cdavis2757@gmail.com>
2023-06-07 11:43:31 -0700

    css-styling

--- js/navbar.js
+++ js/navbar.js
@@ -24,19 +24,3 @@ const navToggle = () => {
 };
 
 navToggle();
-
-
-
-
-//home page image slider//
-// const images = document.querySelectorAll('#slider figure img');
-
-// let currentIndex = 0;
-
-// function showNextImage() {
-//   images[currentIndex].classList.remove('active');
-//   currentIndex = (currentIndex + 1) % images.length;
-//   images[currentIndex].classList.add('active');
-// }
-
-// setInterval(showNextImage, 3000);
